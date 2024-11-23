0ad48684ad40412420a13a03b04a5ce42a25ea98
Asfiyakaunain9 <139836841+Asfiyakaunain9@users.noreply.github.com>
2024-11-23 15:52:27 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function showLogin() {
+    document.getElementById('login-form').classList.remove('hidden');
+    document.getElementById('signup-form').classList.add('hidden');
+  }
+  
+  function showSignup() {
+    document.getElementById('signup-form').classList.remove('hidden');
+    document.getElementById('login-form').classList.add('hidden');
+  }
+  
